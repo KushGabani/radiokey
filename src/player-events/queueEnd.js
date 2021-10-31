f039492bbebeaa67c6d708e81989f18fc629dd27
KushGabani <kushgabz2687@gmail.com>
2021-10-31 12:55:00 +0530

    feat: play and queue messages

---
+++ src/player-events/queueEnd.js
@@ -0,0 +1,12 @@
+module.exports = (client, message, queue) => {
+    message.channel.send({
+        embed: {
+            color: "ORANGE",
+            title: "There are no songs in the queue",
+            footer: {
+                text: "Radiokey | A product by Kush Gabani",
+            },
+            timestamp: new Date(),
+        },
+    });
+};
