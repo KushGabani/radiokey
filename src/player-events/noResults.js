b79243600abd12bc3b4a3f38f58fdf66a31ce0dd
KushGabani <kushgabz2687@gmail.com>
2021-10-31 12:57:44 +0530

    fix: formatting guidelines

--- src/player-events/noResults.js
+++ src/player-events/noResults.js
@@ -1,12 +1,12 @@
 module.exports = (client, message, query) => {
-    message.channel.send({
-        embed: {
-            color: "ORANGE",
-            title: "Cannot find the song",
-            footer: {
-                text: "Radiokey | A product by Kush Gabani",
-            },
-            timestamp: new Date(),
-        },
-    });
+  message.channel.send({
+    embed: {
+      color: "ORANGE",
+      title: "Cannot find the song",
+      footer: {
+        text: "Radiokey | A product by Kush Gabani",
+      },
+      timestamp: new Date(),
+    },
+  });
 };
