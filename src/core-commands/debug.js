26ddc9378594037ab2652bb1aea326f38a0f2458
KushGabani <kushgabz2687@gmail.com>
2021-10-31 10:46:34 +0530

    core: debug bot command

---
+++ src/core-commands/debug.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "debug",
+  aliases: [],
+  category: "core",
+  utilisation: "@debug",
+
+  execute(client, message) {
+    message.channel.send({
+      embed: {
+        color: "ORANGE",
+        title: "Debug",
+        description: {
+          name: `${client.user.username} connected in ${client.voice.connections.size} channels !`,
+        },
+        footer: {
+          text: "Radiokey | A Product by Kush Gabani",
+        },
+        timestamp: new Date(),
+      },
+    });
+  },
+};
