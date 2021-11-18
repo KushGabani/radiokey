a0b5b3315651a4cd9ec4b0e83e3c51f64839b078
alwaysstaycalm24 <shahgatha214@gmail.com>
2021-11-18 13:40:28 +0530

    fix: formatting guidelines

--- src/actions/loop.js
+++ src/actions/loop.js
@@ -2,33 +2,32 @@ const sendMessage = require("./../templates/message");
 const basicChecks = require("../templates/validation");
 
 module.exports = {
-    name: "loop",
-    aliases: ["lp", "loop"],
-    category: "actions",
-    utilisation: "*loop",
+  name: "loop",
+  aliases: ["lp", "loop"],
+  category: "actions",
+  utilisation: "*loop",
 
-    execute(client, message, args) {
-        const response = basicChecks(message);
+  execute(client, message, args) {
+    const response = basicChecks(message);
 
-        if (response) return response;
+    if (response) return response;
 
-
-        if (args.join(" ").toLowerCase() === "queue") {
-            if (client.player.getQueue(message).loopMode) {
-                client.player.setLoopMode(message, false);
-                return sendMessage(message, 'Loop status', `Repeat **disabled**`);
-            } else {
-                client.player.setLoopMode(message, true);
-                return sendMessage(message, 'Loop status', `Repeat **enabled**!`)
-            }
-        } else {
-            if (client.player.getQueue(message).repeatMode) {
-                client.player.setRepeatMode(message, false);
-                return sendMessage(message, 'Loop status', `Repeat **disabled**`);
-            } else {
-                client.player.setRepeatMode(message, true);
-                return sendMessage(message, 'Loop status', `Repeat **enabled**!`)
-            }
-        }
-    },
+    if (args.join(" ").toLowerCase() === "queue") {
+      if (client.player.getQueue(message).loopMode) {
+        client.player.setLoopMode(message, false);
+        return sendMessage(message, "Loop status", `Repeat **disabled**`);
+      } else {
+        client.player.setLoopMode(message, true);
+        return sendMessage(message, "Loop status", `Repeat **enabled**!`);
+      }
+    } else {
+      if (client.player.getQueue(message).repeatMode) {
+        client.player.setRepeatMode(message, false);
+        return sendMessage(message, "Loop status", `Repeat **disabled**`);
+      } else {
+        client.player.setRepeatMode(message, true);
+        return sendMessage(message, "Loop status", `Repeat **enabled**!`);
+      }
+    }
+  },
 };
--- src/actions/stop.js
+++ src/actions/stop.js
@@ -1,30 +1,25 @@
 const basicChecks = require("../templates/validation");
 const sendMessage = require("./../templates/message");
 module.exports = {
-    name: "stop",
-    aliases: ["dc"],
-    category: "actions",
-    utilisation: "*stop | *dc",
+  name: "stop",
+  aliases: ["dc"],
+  category: "actions",
+  utilisation: "*stop | *dc",
 
-    execute(client, message) {
-        const response = basicChecks(message);
+  execute(client, message) {
+    const response = basicChecks(message);
 
-        if (response) return response;
+    if (response) return response;
 
-        if (!client.player.getQueue(message))
-            return sendMessage(message, "Error", "no songs. Cannot pause");
+    if (!client.player.getQueue(message))
+      return sendMessage(message, "Error", "no songs. Cannot pause");
 
-        if (!client.player.getQueue(message).paused)
-            return sendMessage(message, "Error", "Song already resumed!");
+    if (!client.player.getQueue(message).paused)
+      return sendMessage(message, "Error", "Song already resumed!");
 
-        client.player.setRepeatMode(message, false);
-        const success = client.player.stop(message);
+    client.player.setRepeatMode(message, false);
+    const success = client.player.stop(message);
 
-        if (success)
-            sendMessage(
-                message,
-                "Disconnected!",
-                `bot disconnected !`
-            );
-    },
+    if (success) sendMessage(message, "Disconnected!", `bot disconnected !`);
+  },
 };
